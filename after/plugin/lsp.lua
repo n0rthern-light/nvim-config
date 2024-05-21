4346cec5235cdfbc4fdd227bc228d618fdd45e43
ske1337 <ske1337@gmail.com>
2024-05-21 11:25:21 +0200

    Intelephense as php lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -8,7 +8,7 @@ require('mason').setup({})
 require('mason-lspconfig').setup({
 	-- Replace the language servers listed here 
 	-- with the ones you want to install
-	ensure_installed = {'clangd', 'eslint', 'lua_ls', 'phpactor', 'html'},
+	ensure_installed = {'clangd', 'eslint', 'lua_ls', 'intelephense', 'html'},
 	handlers = {
 		function(server_name)
 			require('lspconfig')[server_name].setup({})
